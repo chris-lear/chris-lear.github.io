dadddf12862fe5c289f2f993cc75d0c6003586d0
Chris Lear <chris@barcombe.net>
2020-06-29 21:10:46 +0100

    csv amended to Nick's requirements

--- js/csv.js
+++ js/csv.js
@@ -4,77 +4,111 @@ const urlParams = new URLSearchParams(window.location.search);
 console.log(urlParams);
 let mikeyJamie = urlParams.get('mikey-jamie');
 if (!mikeyJamie) {
-    mikeyJamie = 'mikey-jamie';
+    mikeyJamie = 'Mikey-Jamie';
 }
 let chrisJamie = urlParams.get('chris-jamie');
 if (!chrisJamie) {
-    chrisJamie = 'chris-jamie';
+    chrisJamie = 'Chris-Jamie';
 }
 
 var players = [
     {
         'hapsburg':chrisJamie,
-        'protestant':'mikey',
-        'pope':'filippo',
-        'ottoman':'neil',
-        'france':'ben',
-        'england':'nick'
+        'protestant':'Mikey',
+        'pope':'Filippo',
+        'ottoman':'Neil',
+        'france':'Ben',
+        'england':'Nick'
     },
     {
-        'hapsburg':'nick',
-        'protestant':'ben',
+        'hapsburg':'Nick',
+        'protestant':'Ben',
         'pope':mikeyJamie,
-        'ottoman':'filippo',
-        'france':'neil',
-        'england':'chris'
+        'ottoman':'Filippo',
+        'france':'Neil',
+        'england':'Chris'
     },
     {
-        'hapsburg':'neil',
-        'protestant':'filippo',
-        'pope':'nick',
-        'ottoman':'chris',
+        'hapsburg':'Neil',
+        'protestant':'Filippo',
+        'pope':'Nick',
+        'ottoman':'Chris',
         'france':mikeyJamie,
-        'england':'ben'
+        'england':'Ben'
     },
     {
-        'hapsburg':'jamie',
-        'protestant':'chris',
-        'pope':'ben',
-        'ottoman':'nick',
-        'france':'filippo',
-        'england':'neil'
+        'hapsburg':'Jamie',
+        'protestant':'Chris',
+        'pope':'Ben',
+        'ottoman':'Nick',
+        'france':'Filippo',
+        'england':'Neil'
     }
 ];
 
 
+var Powers = {
+    'hapsburg':'Habsburgs',
+    'protestant':'Protestants',
+    'pope':'Papacy',
+    'ottoman':'Ottomans',
+    'france':'France',
+    'england':'England'
+};
 
 
 $(()=>{
     $('#csv').append("Game,Turn,Nation,Player,Starting VP,VP Change,Dice Rolled,Hits\n");
     var powers = ["ottoman", "hapsburg", "england", "france", "pope", "protestant"];
     var gamenumber = 0;
+    var csv = [];
 
     var show = function(gamenumber) {
-        $.get(`games/game${gamenumber}.txt`, function(data) {
+        return $.get(`games/game${gamenumber}.txt`);
+    }
+    var calls = [];
+    players.forEach((power,count) => {
+        calls.push(show(count));
+    });
+    Promise.all(calls).then((data)=>{
+        data.forEach((d, gamenumber)=> {
+            csv[gamenumber]=[];
             game = new G();
-            game.update(data);
+            game.update(d);
             var delta, lastTurn;
             powers.forEach(power=>{
                 game.Turns.forEach((turn, i) => {
                     var oneTurn = game.extract(power,i);
                     if (i>0) {
                         if (turn.ottoman.cards.length && turn.ottoman.vps) {
+                            if (!csv[gamenumber][i]) {
+                                csv[gamenumber][i] = '';
+                            }
                             delta = oneTurn.vps-lastTurn.vps;
-                            $('#csv').append(`${gamenumber},${i},${power},${players[gamenumber][power]},${lastTurn.vps},${delta},${oneTurn.diceRolled},${oneTurn.hits}\n`);
+                            let Power = Powers[power];
+                            csv[gamenumber][i] += `${gamenumber},${i},${Power},${players[gamenumber][power]},${lastTurn.vps},${delta},${oneTurn.diceRolled},${oneTurn.hits}\n`;
                         }
                     }
                     lastTurn = oneTurn;
                 })
             });
         });
-    }
-    players.forEach((power,count) => {
-        show(count);
+
+        csv[0][5] = `0,5,Ottomans,Neil,16,4,17,5\n`;
+        csv[0][5] += `0,5,Habsburgs,Chris-Jamie,20,4,13,2\n`;
+        csv[0][5] += `0,5,England,Nick,14,3,10,4\n`;
+        csv[0][5] += `0,5,France,Ben,15,5,0,0\n`;
+        csv[0][5] += `0,5,Papacy,Filippo,24,0,12,2\n`;
+        csv[0][5] += `0,5,Protestants,Mikey,12,2,6,3\n`;
+        csv[0][6] = `0,6,Ottomans,Neil,20,7,21,11\n`;
+        csv[0][6] += `0,6,Habsburgs,Chris-Jamie,24,-1,22,7\n`;
+        csv[0][6] += `0,6,England,Nick,17,4,11,4\n`;
+        csv[0][6] += `0,6,France,Ben,20,4,0,0\n`;
+        csv[0][6] += `0,6,Papacy,Filippo,24,3,35,13\n`;
+        csv[0][6] += `0,6,Protestants,Mikey,14,-1,23,12\n`;
+        csv.forEach(l=>{
+            $('#csv').append(l);
+        });
     });
 
 });
